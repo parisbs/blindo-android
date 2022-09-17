73e7e116faca65e7134cd23de12444e99de95909
Paris N. Baltazar Salguero <32491290+parisbs@users.noreply.github.com>
2022-09-16 21:44:51 -0500

    Add debug keystore configuration

--- .github/scripts/generate-keystore-configuration.sh
+++ .github/scripts/generate-keystore-configuration.sh
@@ -1,18 +1,21 @@
 #!/bin/bash -e
 
 storeFilePath="${GITHUB_WORKSPACE}/${STORE_FILE}"
+debugStoreFilePath="${GITHUB_WORKSPACE}/debug_${STORE_FILE}"
 
 echo "Generating store file in ${storeFilePath}"
 echo ${ENCODED_STORE} | base64 --decode > ${storeFilePath}
+echo "Generating debug store file in ${debugStoreFilePath}"
+echo ${DEBUG_ENCODED_STORE} | base64 --decode > ${debugStoreFilePath}
 
 keystorePropertiesPath="${GITHUB_WORKSPACE}/keystore.properties"
 
 echo "Generating keystore properties in ${keystorePropertiesPath}"
 touch ${keystorePropertiesPath}
-echo "DEBUG_STORE_FILE=${storeFilePath}" >> ${keystorePropertiesPath}
-echo "DEBUG_STORE_PASSWORD=${STORE_PASSWORD}" >> ${keystorePropertiesPath}
-echo "DEBUG_KEY_ALIAS=${KEY_ALIAS}" >> ${keystorePropertiesPath}
-echo "DEBUG_KEY_PASSWORD=${KEY_PASSWORD}" >> ${keystorePropertiesPath}
+echo "DEBUG_STORE_FILE=${debugStoreFilePath}" >> ${keystorePropertiesPath}
+echo "DEBUG_STORE_PASSWORD=${DEBUG_STORE_PASSWORD}" >> ${keystorePropertiesPath}
+echo "DEBUG_KEY_ALIAS=${DEBUG_KEY_ALIAS}" >> ${keystorePropertiesPath}
+echo "DEBUG_KEY_PASSWORD=${DEBUG_KEY_PASSWORD}" >> ${keystorePropertiesPath}
 echo "STORE_FILE=${storeFilePath}" >> ${keystorePropertiesPath}
 echo "STORE_PASSWORD=${STORE_PASSWORD}" >> ${keystorePropertiesPath}
 echo "KEY_ALIAS=${KEY_ALIAS}" >> ${keystorePropertiesPath}
